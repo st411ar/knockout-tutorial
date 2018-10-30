774ecea1aa7f1d28b06400cad988602a6442ce4b
Vitaly Kiselev <st411ar@ya.ru>
2018-10-30 19:41:58 +0300

    introduction, step 5, completed

--- introduction/view-model.js
+++ introduction/view-model.js
@@ -1,12 +1,14 @@
 function AppViewModel() {
   this.firstName = ko.observable("Bert");
   this.lastName = ko.observable("Bertington");
+
   this.fullName = ko.computed(
     function() {
       return this.firstName() + " " + this.lastName();
     },
     this
   );
+  
   this.capitalizeLastName = function() {
     var currentVal = this.lastName();
     this.lastName(currentVal.toUpperCase());
