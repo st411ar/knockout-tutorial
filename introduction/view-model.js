044e72e9c57b1e5ce9664f727597b288a8357e49
Vitaly Kiselev <st411ar@ya.ru>
2018-10-30 19:31:55 +0300

    introduction, step 3, computed

--- introduction/view-model.js
+++ introduction/view-model.js
@@ -1,4 +1,10 @@
 function AppViewModel() {
   this.firstName = ko.observable("Bert");
   this.lastName = ko.observable("Bertington");
+  this.fullName = ko.computed(
+    function() {
+      return this.firstName() + " " + this.lastName();
+    },
+    this
+  );
 }
