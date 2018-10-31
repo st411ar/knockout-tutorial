39f7ca3829d338cc1e64733ae3c687a4e720b344
Vitaly Kiselev <st411ar@ya.ru>
2018-10-31 20:07:02 +0300

    Single Page App, step 1, implemented folder selecting

--- single-page-app/view-model.js
+++ single-page-app/view-model.js
@@ -1,9 +1,16 @@
 function WebmailViewModel() {
   var self = this;
-  this.folders = [
+
+  self.folders = [
     "Inbox",
     "Archive",
     "Sent",
     "Spam"
   ];
+
+  self.chosenFolderId = ko.observable();
+
+  self.goToFolder = function(folder) {
+    self.chosenFolderId(folder);
+  }
 }
