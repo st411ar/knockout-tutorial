ce2cbe630ae19644d8560968d9161ac29a25d88a
Vitaly Kiselev <st411ar@ya.ru>
2018-11-02 14:25:26 +0300

    Custom Bind, step 1, created simple dynamic survey

---
+++ custom-bind/script.js
@@ -0,0 +1,12 @@
+ko.applyBindings(
+  new SurveyViewModel(
+    "Which factors affect your technology choices?",
+    10,
+    [
+      "Functionality, compatibility, pricing - all that boring stuff",
+      "How often it is mentioned on Hacher News",
+      "Number of gradients/dropshadows on project homepage",
+      "Totally believable testimonials on project homepage"
+    ]
+  )
+);
---
+++ custom-bind/view-model.js
@@ -0,0 +1,27 @@
+function Answer(text) {
+  this.answerText = text;
+  this.points = ko.observable(1);
+}
+
+function SurveyViewModel(question, pointsBudget, answers) {
+  this.question = question;
+  this.pointsBudget = pointsBudget;
+  this.answers = $.map(
+    answers,
+    function(text) {
+      return new Answer(text);
+    }
+  );
+
+  this.save = function() {
+    alert('To do');
+  }
+
+  this.pointsUsed = ko.computed(function() {
+    var total = 0;
+    for (var i = 0; i < this.answers.length; i++) {
+      total += this.answers[i].points();
+    }
+    return total;
+  }, this);
+}
