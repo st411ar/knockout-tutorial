e20413d627cf54c3964d4706c74c51acb607d657
Vitaly Kiselev <st411ar@ya.ru>
2018-11-05 11:16:27 +0300

    Load and save data, step 1, init tasks list

---
+++ load-save-data/view-model.js
@@ -0,0 +1,28 @@
+function Task(data) {
+  this.title = ko.observable(data.title);
+  this.isDone = ko.observable(data.isDone);
+}
+
+function TaskListViewModel() {
+  var self = this;
+  self.tasks = ko.observableArray([]);
+  self.newTaskText = ko.observable();
+
+  self.incompleteTasks = ko.computed(function() {
+    return ko.utils.arrayFilter(
+      self.tasks(),
+      function(task) {
+        return !task.isDone();
+      }
+    );
+  });
+
+  self.addTask = function() {
+    self.tasks.push(new Task({ title: this.newTaskText() }));
+    self.newTaskText('');
+  };
+
+  self.removeTask = function(task) {
+    self.tasks.remove(task);
+  };
+}
